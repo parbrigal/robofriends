de94a0f0ff5e084c81630e8113485ebfd406130c
parbrigal <30548384+parbrigalDev@users.noreply.github.com>
2018-06-25 06:03:34 +0200

    Initial Commit

---
+++ src/components/Scroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scroll = (props) => {
+  return (
+    <div style={{border:'1px solid black',height:'1500px'}}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Scroll;
